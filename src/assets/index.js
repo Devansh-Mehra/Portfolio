09a600f7e4a89b5070d758b5e601cc41c026d32a
Devansh Mehra <81522384+Devansh-Mehra@users.noreply.github.com>
2023-09-18 21:00:56 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import backend from './backend.png';
+import smart from './smart.png';
+import blockchain from './blockchain.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import css from './tech/css.png';
+import solidity from './tech/solidity.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import hardhat from './tech/hardhat.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import sql from './tech/sql.png';
+import threejs from './tech/threejs.svg';
+
+import samsung from './company/samsung.png';
+import magazine3 from './company/magazine3.png';
+import ejyhealth from './company/ejyhealth.png';
+
+import ornamint from './ornamint.png';
+import medbase from './medbase.png';
+import ticket from './ticket.png';
+
+export {
+  logo,
+  backend,
+  smart,
+  blockchain,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  solidity,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  hardhat,
+  reactjs,
+  redux,
+  tailwind,
+  sql,
+  threejs,
+  samsung,
+  magazine3,
+  ejyhealth,
+  ornamint,
+  medbase,
+  ticket,
+};
